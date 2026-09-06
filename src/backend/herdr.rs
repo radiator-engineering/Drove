0829d256321201000452124249d7fa37835298dd
Jacob Martin <martinjaco@gmail.com>
2026-09-06 11:33:50 -0400

    backend: drop redundant trait imports in herdr test module

--- src/backend/herdr.rs
+++ src/backend/herdr.rs
@@ -465,10 +465,7 @@ fn connect(path: &Path) -> std::io::Result<Stream> {
 
 #[cfg(test)]
 mod tests {
-    use std::{
-        io::{BufRead as _, BufReader, Write as _},
-        thread,
-    };
+    use std::thread;
 
     use interprocess::local_socket::{Listener, ListenerOptions, traits::Listener as _};
 
